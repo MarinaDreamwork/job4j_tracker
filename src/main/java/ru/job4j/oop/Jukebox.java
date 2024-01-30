9c55406c999bf39058690990a4719bd08b964efc
Trushina Marina <silaevamo@gmail.com>
2024-01-30 09:53:24 +0300

    1.3. Вызов метода с аргументами. [#166086]

---
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -0,0 +1,20 @@
+package ru.job4j.oop;
+
+public class Jukebox {
+
+    public void music(int position) {
+        String song = switch (position) {
+            case 1 -> "Пусть бегут неуклюже";
+            case 2 -> "Спокойной ночи";
+            default -> "Песня не найдена";
+        };
+        System.out.println(song);
+    }
+
+    public static void main(String[] args) {
+        Jukebox song = new Jukebox();
+        song.music(1);
+        song.music(2);
+        song.music(5);
+    }
+}
