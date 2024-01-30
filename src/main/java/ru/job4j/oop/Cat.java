912d5954e6d5aa0eff9266040627e823559e262e
Trushina Marina <silaevamo@gmail.com>
2024-01-30 10:21:44 +0300

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,9 +1,32 @@
 package ru.job4j.oop;
 
 public class Cat {
+    private String food;
+    private  String name;
+
+    public void show() {
+        System.out.println(this.name + " eats " + this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
+        System.out.println("Here's Gav's food");
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.giveNick("Gav");
+        gav.show();
+        System.out.println("Here's Black's food");
+        Cat black = new Cat();
+        black.eat("fish");
+        black.giveNick("Black cat");
+        black.show();
     }
 
 }
