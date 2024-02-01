8b7f7efbabd45196bd3446a73a35bebe9f6ef501
Trushina Marina <silaevamo@gmail.com>
2024-02-01 09:50:03 +0300

    4. Переопределение метода [#174890]

---
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,14 @@
+package ru.job4j.inheritance;
+
+public class JSONReport extends TextReport {
+
+    public String generate(String name, String body) {
+        String lineSeparator = System.lineSeparator();
+        return '{' + lineSeparator
+                + "\t" + "\"name\"" + ' ' + ':' + ' ' + '"' + name + '"' + ','
+                + lineSeparator
+                + "\t" + "\"body\"" + ' ' + ':' + ' ' + '"' + body + '"'
+                + lineSeparator
+                + '}';
+    }
+}
