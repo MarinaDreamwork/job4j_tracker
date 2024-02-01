5759db6fe4212bac8081745885d1f8d1d5481003
Trushina Marina <silaevamo@gmail.com>
2024-02-01 21:29:41 +0300

    7. Аннотация @Override [#174891]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -2,6 +2,8 @@ package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
 
+    @Override
+
     public String generate(String name, String body) {
         String lineSeparator = System.lineSeparator();
         return '{' + lineSeparator
