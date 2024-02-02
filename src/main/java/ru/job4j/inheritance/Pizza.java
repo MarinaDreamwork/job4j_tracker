ef0f3ab095aaaa18e705d225da2a6e07cca97e5a
Trushina Marina <silaevamo@gmail.com>
2024-02-02 09:54:15 +0300

    6. Вызов переопределенного метода родителя - super.method ... [#504871]

---
+++ src/main/java/ru/job4j/inheritance/Pizza.java
@@ -0,0 +1,13 @@
+package ru.job4j.inheritance;
+
+public class Pizza {
+    private String name;
+
+    public Pizza() {
+        this.name = "Just tasty pizza";
+    }
+
+    public String name() {
+        return this.name;
+    }
+}
