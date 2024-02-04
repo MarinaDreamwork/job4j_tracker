3a238b1da69a34a7079e3c4b823b77a01ddffc3e
Trushina Marina <silaevamo@gmail.com>
2024-02-04 12:32:20 +0300

    6. Метод замены заявки. Tracker.replace [#211748 #478894] - commit log to fix

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -49,7 +49,6 @@ public class Tracker {
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
         if (index != -1) {
-
             item.setId(id);
             items[index] = item;
         }
