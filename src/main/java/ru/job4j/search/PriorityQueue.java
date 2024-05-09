6ef7d3a44e0db80f2c46b4952a4e9ef60d78b589
Trushina Marina <silaevamo@gmail.com>
2024-05-09 16:54:32 +0300

    5. Очередь с приоритетом на LinkedList [#41670]

---
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -0,0 +1,22 @@
+package ru.job4j.search;
+
+import java.util.LinkedList;
+
+public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+    public void put(Task task) {
+        int index = 0;
+        int priority = task.getPriority();
+        for (Task element: tasks) {
+            if (priority >= element.getPriority()) {
+                index++;
+            }
+        }
+        this.tasks.add(index, task);
+    }
+
+    public Task take() {
+        return tasks.poll();
+    }
+}
