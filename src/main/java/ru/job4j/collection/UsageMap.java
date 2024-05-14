57755d9872b316cfa14b4cc035a010ab54fcffc4
Trushina Marina <silaevamo@gmail.com>
2024-05-14 17:57:02 +0300

    0. Map, HashMap [#212652 #495999] - fixed by code review

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,8 @@ import java.util.Map;
 public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> data = new HashMap<>();
+        data.put("silaevamo@gamil.com", "Трушина Мария Олеговна");
+        data.put("silaeva@mail.ru", "Силаева Марина Олеговна");
         data.put("silaevamo@gamil.com", "Трушина Марина Олеговна");
         for (Map.Entry<String, String> element : data.entrySet()) {
             String key = element.getKey();
