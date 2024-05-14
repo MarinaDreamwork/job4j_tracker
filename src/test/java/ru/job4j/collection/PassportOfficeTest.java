09842e609436b7dfab9dc4249bab2c6191782fbe
Trushina Marina <silaevamo@gmail.com>
2024-05-14 18:26:04 +0300

    1. Паспорт и жители [#212656]

---
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PassportOfficeTest {
+    @Test
+    public void whenTestAddMethodTrue() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport())).isEqualTo(citizen);
+    }
+
+    @Test
+    public void whenTestAddMethodIsFalse() {
+        Citizen citizen = new Citizen("65Ikdc4", "Max Maximov");
+        Citizen anotherCitizen = new Citizen("328Jg", "Ivan Ivanov");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.add(anotherCitizen)).isFalse();
+    }
+}
