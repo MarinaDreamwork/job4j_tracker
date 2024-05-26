74128ec842f1f86ab54df522266e481e5414aa8f
Trushina Marina <silaevamo@gmail.com>
2024-05-26 12:33:58 +0300

    0. Сортировка [#10096 #497936]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -3,7 +3,7 @@ package ru.job4j.tracker;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class Item implements Comparable<Item>{
+public class Item implements Comparable<Item> {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
     private int id;
     private String name;
